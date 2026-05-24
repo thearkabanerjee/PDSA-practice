97803e8320e24a33ea4ef78eefab3b0aa86ac3ee
Arka Banerjee <arkabanerjee381@gmail.com>
2026-05-24 16:44:21 +0530

    c++ dsa

---
+++ week-1/c++/first5alphabets.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    char a = 'a';
+    for (int i = 1; i <= 5; i++){
+        cout << a << endl;
+        a += 1;
+    }
+    return 0;
+}
