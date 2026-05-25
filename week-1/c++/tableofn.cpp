435f7c3e5ac5d996aeb7786320254ef2308b150c
Arka Banerjee <arkabanerjee381@gmail.com>
2026-05-25 21:46:31 +0530

    n table

---
+++ week-1/c++/tableofn.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+
+    cout << a << " * 1 = " << 6*1 << endl;
+    cout << a << " * 2 = " << 6*2 << endl;
+    cout << a << " * 3 = " << 6*3 << endl;
+    cout << a << " * 4 = " << 6*4 << endl;
+    cout << a << " * 5 = " << 6*5 << endl;
+    cout << a << " * 6 = " << 6*6 << endl;
+    cout << a << " * 7 = " << 6*7 << endl;
+    cout << a << " * 8 = " << 6*8 << endl;
+    cout << a << " * 9 = " << 6*9 << endl;
+    cout << a << " * 10 = " << 6*10 << endl;
+
+
+    return 0;
+}
