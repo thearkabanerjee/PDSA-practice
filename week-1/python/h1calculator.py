ddb7fc6afa83ca4b97f8b9f3cc59bda5e1bb1007
Arka Banerjee <arkabanerjee381@gmail.com>
2026-05-25 22:03:43 +0530

    h1calculator

---
+++ week-1/python/h1calculator.py
@@ -0,0 +1,9 @@
+number1= int(input())
+number2= int(input())
+
+
+print (f"{number1} + {number2} = {number1 + number2}")
+print (f"{number1} - {number2} = {number1 - number2}")
+print (f"{number1} * {number2} = {number1 * number2}")
+print (f"{number1} / {number2} = {number1 // number2}")
+print (f"{number1} % {number2} = {number1 % number2}")
