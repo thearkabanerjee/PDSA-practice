0c857204a692362788d49e9e3a7f46ee938c5b85
Arka Banerjee <arkabanerjee381@gmail.com>
2026-05-27 10:15:51 +0530

    h2calculator

---
+++ week-1/c++/h2calculator.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    long long n,m;
+    cin >> n >> m;
+    cout << n << " + " << m << " = "<< n+m << "\n\n";
+    cout << n << " - " << m << " = "<< n-m << "\n\n";
+    cout << n << " * " << m << " = "<< n*m << "\n\n";
+    cout << n << " / " << m << " = "<< n/m << "\n\n";
+    cout << n << " % " << m << " = "<< n%m << "\n";
+    return 0;
+}
