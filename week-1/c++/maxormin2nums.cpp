b0057a7ec904a06071bd1df8d22f7beb268b4eea
Arka Banerjee <arkabanerjee381@gmail.com>
2026-05-27 12:18:15 +0530

    max or min 2 nums

---
+++ week-1/c++/maxormin2nums.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a>> b;
+
+
+    cout << "Min = " << min(a, b) << endl;
+    cout << "Max = " << max(a, b) << endl;
+
+    return 0;
+}
