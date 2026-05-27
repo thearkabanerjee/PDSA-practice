c92be79a34af052bae377bd48f41fed0ee8e58d9
Arka Banerjee <arkabanerjee381@gmail.com>
2026-05-27 13:00:13 +0530

    location point

---
+++ week-1/c++/location-point.cpp
@@ -0,0 +1,31 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    int x, y;
+    cin >> x>> y;
+
+    if (x == 0 and y == 0){
+        cout << "Origin" << endl;
+    }
+    else if (x == 0 and y!=0){
+        cout << "Y axis" << endl;
+    }
+    else if (x != 0 and y == 0){
+        cout << "X axis" << endl;
+    }
+    else if (x > 0 and y > 0){
+        cout << "1st Quadrant" << endl;
+    }
+    else if (x < 0 and y > 0){
+        cout << "2nd Quadrant" << endl;
+    }
+    else if(x<0 and y <0){
+        cout << "3rd Quadrant" << endl;
+    }
+    else if (x>0 and y<0){
+        cout << "4th Quadrant"<< endl;
+    }
+    return 0;
+}
