22fea4a4e913db23ca9b15a5678566ac5a6f71a6
Arka Banerjee <arkabanerjee381@gmail.com>
2026-05-27 11:17:06 +0530

    pass or fail

---
+++ week-1/c++/passorfail.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    
+    if (a >= 35){
+        cout << "Pass" << endl;
+    }
+    else {
+        cout << "Fail" << endl;
+    }
+
+    return 0;
+}
