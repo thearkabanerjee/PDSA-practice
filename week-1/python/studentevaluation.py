14b826d415cd282c550da3ea1258004e5fe40d21
Arka Banerjee <arkabanerjee381@gmail.com>
2026-05-27 12:35:31 +0530

    student evaluation

---
+++ week-1/python/studentevaluation.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+if (a >90):
+    print ("Excellent")
+elif (a > 80 and a<= 90):
+    print("Good")
+elif(a > 70 and a <= 80 ):
+    print("Fair")
+elif(a >60 and a<= 70):
+    print("Meets Expectations")
+elif(a <= 60):
+    print("Below Par")
