acd7230dc5964094f57c227e55a94357ae370c34
Arka Banerjee <arkabanerjee381@gmail.com>
2026-05-28 12:02:09 +0530

    print n to 1

---
+++ week-2/c++/printnto1.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    int a ;
+    cin >> a;
+
+    for (int i = a; i >= 1; i--){
+        cout << i << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
