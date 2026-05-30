a0e278859db5cd3fca0a0cf1dc2453086442a8fb
Arka Banerjee <arkabanerjee381@gmail.com>
2026-05-30 13:30:21 +0530

    power

--- week-2/c++/power.cpp
+++ week-2/c++/power.cpp
@@ -6,9 +6,14 @@ int main(){
     long long multiple = 1;
     cin >> a>> b;
 
-    for (int i = 0; i < b; i++){
+    if (b == 0){
+        multiple = 1;
+    }else {
+        for (int i = 1; i <= b; i++){
         multiple *= a;
+        }
     }
+    
 
     cout << multiple << endl;
     return 0;
