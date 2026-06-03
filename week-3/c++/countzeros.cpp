8bb66a61771837db8c2749ce9d0e07270e2fb61a
Arka Banerjee <arkabanerjee381@gmail.com>
2026-06-03 17:25:39 +0530

    count zeros

--- week-3/c++/countzeros.cpp
+++ week-3/c++/countzeros.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 
 int main(){
-    long long a;
+    string a;
     cin >> a;
 
+    int count = 0;
 
+    for (int i = 0; i < a.size(); i++){
+        if (a[i] == '0'){
+            count += 1;
+        }
+    }
     // write rest here
 
 
-
+    cout << count << endl;
 
     return 0;
 }
