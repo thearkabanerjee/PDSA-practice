cc94033b2c9871c6ffeb4788b539083118f5162b
Arka Banerjee <arkabanerjee381@gmail.com>
2026-07-05 02:35:17 +0530

    first alphabets

---
+++ new-week1/firstalphabets.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+  
+  char alphabet = 'A' ;
+  for (int i = 0; i < 5; i++){
+    cout << alphabet << endl;
+    alphabet ++;
+  }
+
+
+  return 0;
+}
