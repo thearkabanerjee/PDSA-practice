993650378218ef9645774a7de5e9c915019156f9
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-07-08 03:13:03 +0530

    c++ : question 6

---
+++ contest-2/question-6.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    
+
+    for (char a = 'A' ; a <= 'Z'; a++){
+        cout << a << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
